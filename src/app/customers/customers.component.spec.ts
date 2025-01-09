bdbac9ba0bfe7058e12bca5fda8b7ca702aadd62
Aldric Adams <aldricadams12@gmail.com>
2025-01-09 09:31:19 +0100

    update

--- src/app/customers/customers.component.spec.ts
+++ src/app/customers/customers.component.spec.ts
@@ -1,26 +1,46 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { CustomersComponent } from './customers.component';
+import { DestroyRef } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { provideRouter } from '@angular/router';
+import { of } from 'rxjs';
+
+import { CustomersComponent } from './customers.component';
 import { CustomersService } from './customers.service';
 
 describe('CustomersComponent', () => {
   let component: CustomersComponent;
   let fixture: ComponentFixture<CustomersComponent>;
-  let customersService: CustomersService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [CustomersComponent],
-      providers: [provideHttpClient(), provideHttpClientTesting(), provideRouter([])],
-    })
-    .compileComponents();
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        provideRouter([]),
+        {
+          provide: CustomersService,
+          useValue: {
+            getCustomers: () =>
+              of([
+                {
+                  customer_id: 1,
+                  name: 'John Doe',
+                  city: 'City',
+                },
+              ]),
+          },
+        },
+        // {
+        //   provide: DestroyRef,
+        //   useValue: DestroyRef.onDestroy(),
+        // },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CustomersComponent);
     component = fixture.componentInstance;
-    customersService = TestBed.inject(CustomersService);
     fixture.detectChanges();
   });
 
@@ -29,19 +49,152 @@ describe('CustomersComponent', () => {
   });
 
   describe('ngOnInit', () => {
-    it('should fetch customers and update the signal + dataSource', () => {
-      // expect(received).toHaveBeenCalled()
-      // Matcher error: received value must be a mock or spy function
-
-      expect(customersService.getCustomers()).toHaveBeenCalled();
-      expect(component.customers()).toEqual([{ customer_id: 1, name: 'John Doe', city: 'City' }]);
-      expect(component.dataSource.data).toEqual([{ customer_id: 1, name: 'John Doe', city: 'City' }]);
+    it('should fetch customers', () => {
+      component.ngOnInit();
+      expect(component.customers()).toEqual([
+        { customer_id: 1, name: 'John Doe', city: 'City' },
+      ]);
+      expect(component.dataSource.data).toEqual([
+        { customer_id: 1, name: 'John Doe', city: 'City' },
+      ]);
     });
-    // it('should set params for sorting on ID and Name', () => {
-    //   expect();
-    // });
-    // it('should clean up subscriptions', () => {
-    //   expect();
-    // });
   });
+
+  describe('sortOnId', () => {
+    beforeEach(() => {
+      component.customers.set([
+        {
+          customer_id: 2,
+          name: 'John Doe',
+          city: 'City',
+        },
+        {
+          customer_id: 1,
+          name: 'John Doe',
+          city: 'City',
+        },
+        {
+          customer_id: 3,
+          name: 'John Doe',
+          city: 'City',
+        },
+      ]);
+    });
+
+    it('should set params for sorting on ID and Name `default`', () => {
+      expect(component.sortOnId()[0].customer_id).toBe(3);
+      expect(component.sortOnId()[1].customer_id).toBe(2);
+      expect(component.sortOnId()[2].customer_id).toBe(1);
+      expect(component.sortOnId()).toEqual([
+        {
+          customer_id: 3,
+          name: 'John Doe',
+          city: 'City',
+        },
+        {
+          customer_id: 2,
+          name: 'John Doe',
+          city: 'City',
+        },
+        {
+          customer_id: 1,
+          name: 'John Doe',
+          city: 'City',
+        },
+      ]);
+    });
+
+    it('should set params for sorting on ID and Name `asc`', () => {
+      component.sortId.set('asc');
+      expect(component.sortOnId()).toEqual([
+        {
+          customer_id: 1,
+          name: 'John Doe',
+          city: 'City',
+        },
+        {
+          customer_id: 2,
+          name: 'John Doe',
+          city: 'City',
+        },
+        {
+          customer_id: 3,
+          name: 'John Doe',
+          city: 'City',
+        },
+      ]);
+    });
+  });
+
+  describe('sortOnName', () => {
+    beforeEach(() => {
+      component.customers.set([
+        {
+          customer_id: 1,
+          name: 'John Doe',
+          city: 'City',
+        },
+        {
+          customer_id: 2,
+          name: 'Fohn Doe',
+          city: 'City',
+        },
+        {
+          customer_id: 3,
+          name: 'Gohn Doe',
+          city: 'City',
+        },
+      ]);
+    });
+
+    it('should set params for sorting on Name `default`', () => {
+      // expect(component.sortOnId()[0].name).toBe(3);
+      // expect(component.sortOnId()[1].name).toBe(2);
+      // expect(component.sortOnId()[2].name).toBe(1);
+      expect(component.sortOnName()).toEqual([
+        {
+          customer_id: 1,
+          name: 'John Doe',
+          city: 'City',
+        },
+        {
+          customer_id: 3,
+          name: 'Gohn Doe',
+          city: 'City',
+        },
+        {
+          customer_id: 2,
+          name: 'Fohn Doe',
+          city: 'City',
+        },
+      ]);
+    });
+
+    it('should set params for sorting on Name `asc`', () => {
+      component.sortName.set('asc');
+      expect(component.sortOnName()).toEqual([
+        {
+          customer_id: 2,
+          name: 'Fohn Doe',
+          city: 'City',
+        },
+        {
+          customer_id: 3,
+          name: 'Gohn Doe',
+          city: 'City',
+        },
+        {
+          customer_id: 1,
+          name: 'John Doe',
+          city: 'City',
+        },
+      ]);
+    });
+  });
+
+  // describe('onDestroy', () => {
+  //   it('should clean up subscriptions', () => {
+  //     component.ngOnDestroy();
+  //   });
+  // });
 });
